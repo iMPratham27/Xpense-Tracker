d6ec31dd212f4c2eb8104033348ab2efe0a6fb8f
Prathamesh <prathameshdhadankar07@gmail.com>
2025-10-17 19:01:52 +0530

    update sendMail

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -3,6 +3,7 @@ import { userModel } from "../model/userModel.js";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
